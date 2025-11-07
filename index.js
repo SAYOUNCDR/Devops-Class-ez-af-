542b78888aa2f68d032faec2e17e65100d767bad
SAYOUNCDR <pheonixgamer12345@gmail.com>
2025-11-07 13:34:38 +0530

    chnaged the braces

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // This is the test file for
 //  git init
-// git add . {to stage all files}
+// git add . [this stages all changes in the current directory]
 // git commit -m "Initial commit" [this saves the changes to the local repository]
 // git remote add origin <remote_repository_URL> [this links the local repository to a remote repository]
 
