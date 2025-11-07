b43772ce12b9e19c73ae9945463561770137b565
SAYOUNCDR <pheonixgamer12345@gmail.com>
2025-11-07 13:25:58 +0530

    Addedd instruction

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// This is the test file for 
+// This is the test file for
+//  git init
+// git add . {to stage all files}
+// git commit -m "Initial commit" [this saves the changes to the local repository]
+// git remote add origin <remote_repository_URL> [this links the local repository to a remote repository]
+
+// git push -u origin main [this pushes the committed changes to the remote repository]
