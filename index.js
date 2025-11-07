2fd8f6ec338507c0a7cd926a18eed8de7d0fb8e8
SAYOUNCDR <pheonixgamer12345@gmail.com>
2025-11-07 13:46:17 +0530

    Added and modified

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 // git add . [this stages all changes in the current directory]
 // git commit -m "Initial commit" [this saves the changes to the local repository]
 // git remote add origin <remote_repository_URL> [this links the local repository to a remote repository]
-
 // git push -u origin main [this pushes the committed changes to the remote repository]
+// git status [this shows the current status of the repository]
